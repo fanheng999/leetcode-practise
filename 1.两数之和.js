cddd59f05cfbc1973621739cd88342dc65514db4
baibaipangpang <778099013@qq.com>
2023-04-16 20:01:12 +0800

    update 1 1

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -11,8 +11,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
-    
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				return [i, j]
+			}
+		}
+	}
 };
 // @lc code=end
 
