f2945501b72b04108d641b72b671e7ae8d123f7e
baibaipangpang <778099013@qq.com>
2023-04-16 20:57:06 +0800

    update 1 2

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,12 +10,24 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] === target) {
-				return [i, j]
-			}
+// var twoSum = function(nums, target) {
+// 	for (let i = 0; i < nums.length; i++) {
+// 		for (let j = i + 1; j < nums.length; j++) {
+// 			if (nums[i] + nums[j] === target) {
+// 				return [i, j]
+// 			}
+// 		}
+// 	}
+// };
+var twoSum = function (nums, target) {
+	let obj = {}
+	for (let i = 0; i < nums.length; i++){
+		obj[nums[i]] = i
+	}
+	for (let i = 0; i < nums.length; i++){
+		const cha = target - nums[i]
+		if (obj[cha] && i !== obj[cha]) {
+			return [i, obj[cha]]
 		}
 	}
 };
